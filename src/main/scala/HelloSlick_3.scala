36b03ed50ab5123288924d76c3da0a4e7915a3e5
wevogt <11Frisco>
2018-01-11 07:09:27 +0100

    mit ExecutionContext

--- src/main/scala/HelloSlick_3.scala
+++ src/main/scala/HelloSlick_3.scala
@@ -3,6 +3,9 @@
 import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 
+import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.duration.Duration
+
 //import slick.driver.H2Driver.api._
 //import slick.driver.PostgresDriver.api._
 //import slick.driver.HsqldbDriver.api._
@@ -23,6 +26,7 @@ object HelloSlick_3 extends App {
 
   import scala.concurrent.Future
 
+  implicit val ec = scala.concurrent.ExecutionContext.global
   try {
 
     // The query interface for the Suppliers table
@@ -43,15 +47,14 @@ object HelloSlick_3 extends App {
     )
 
     val setupFuture: Future[Unit] = dbConfig.db.run(setupAction)
-/*
     val f = setupFuture.flatMap { _ =>
 
       // Insert some coffees (using JDBC's batch insert feature)
-      val insertAction: DBIO[Option[Int]] = coffees ++= Seq (
-        ("Colombian",         101, 7.99, 0, 0),
-        ("French_Roast",       49, 8.99, 0, 0),
-        ("Espresso",          150, 9.99, 0, 0),
-        ("Colombian_Decaf",   101, 8.99, 0, 0),
+      val insertAction: DBIO[Option[Int]] = coffees ++= Seq(
+        ("Colombian", 101, 7.99, 0, 0),
+        ("French_Roast", 49, 8.99, 0, 0),
+        ("Espresso", 150, 9.99, 0, 0),
+        ("Colombian_Decaf", 101, 8.99, 0, 0),
         ("French_Roast_Decaf", 49, 9.99, 0, 0)
       )
 
@@ -69,144 +72,146 @@ object HelloSlick_3 extends App {
         insertAndPrintAction >> allSuppliersAction
 
       val combinedFuture: Future[Seq[(Int, String, String, String, String, String)]] =
-        db.run(combinedAction)
+        dbConfig.db.run(combinedAction)
 
       combinedFuture.map { allSuppliers =>
         allSuppliers.foreach(println)
       }
+    } (ec)
 
-    }.flatMap { _ =>
+      /*
+          }.flatMap { _ =>
 
-      /* Streaming */
+            /* Streaming */
 
-      val coffeeNamesAction: StreamingDBIO[Seq[String], String] =
-        coffees.map(_.name).result
+            val coffeeNamesAction: StreamingDBIO[Seq[String], String] =
+              coffees.map(_.name).result
 
-      val coffeeNamesPublisher: DatabasePublisher[String] =
-        dbConfig.db.stream(coffeeNamesAction)
+            val coffeeNamesPublisher: DatabasePublisher[String] =
+              dbConfig.db.stream(coffeeNamesAction)
 
-      coffeeNamesPublisher.foreach(println)
+            coffeeNamesPublisher.foreach(println)
 
-    }.flatMap { _ =>
+          }.flatMap { _ =>
 
-      /* Filtering / Where */
+            /* Filtering / Where */
 
-      // Construct a query where the price of Coffees is > 9.0
-      val filterQuery: Query[Coffees, (String, Int, Double, Int, Int), Seq] =
-        coffees.filter(_.price > 9.0)
+            // Construct a query where the price of Coffees is > 9.0
+            val filterQuery: Query[Coffees, (String, Int, Double, Int, Int), Seq] =
+              coffees.filter(_.price > 9.0)
 
-      // Print the SQL for the filter query
-      println("Generated SQL for filter query:\n" + filterQuery.result.statements)
+            // Print the SQL for the filter query
+            println("Generated SQL for filter query:\n" + filterQuery.result.statements)
 
-      // Execute the query and print the Seq of results
-      db.run(filterQuery.result.map(println))
+            // Execute the query and print the Seq of results
+            db.run(filterQuery.result.map(println))
 
-    }.flatMap { _ =>
+          }.flatMap { _ =>
 
-      /* Update */
+            /* Update */
 
-      // Construct an update query with the sales column being the one to update
-      val updateQuery: Query[Rep[Int], Int, Seq] = coffees.map(_.sales)
+            // Construct an update query with the sales column being the one to update
+            val updateQuery: Query[Rep[Int], Int, Seq] = coffees.map(_.sales)
 
-      val updateAction: DBIO[Int] = updateQuery.update(1)
+            val updateAction: DBIO[Int] = updateQuery.update(1)
 
-      // Print the SQL for the Coffees update query
-      println("Generated SQL for Coffees update:\n" + updateQuery.updateStatement)
+            // Print the SQL for the Coffees update query
+            println("Generated SQL for Coffees update:\n" + updateQuery.updateStatement)
 
-      // Perform the update
-      db.run(updateAction.map { numUpdatedRows =>
-        println(s"Updated $numUpdatedRows rows")
-      })
+            // Perform the update
+            db.run(updateAction.map { numUpdatedRows =>
+              println(s"Updated $numUpdatedRows rows")
+            })
 
-    }.flatMap { _ =>
+          }.flatMap { _ =>
 
-      /* Delete */
+            /* Delete */
 
-      // Construct a delete query that deletes coffees with a price less than 8.0
-      val deleteQuery: Query[Coffees,(String, Int, Double, Int, Int), Seq] =
-        coffees.filter(_.price < 8.0)
+            // Construct a delete query that deletes coffees with a price less than 8.0
+            val deleteQuery: Query[Coffees,(String, Int, Double, Int, Int), Seq] =
+              coffees.filter(_.price < 8.0)
 
-      val deleteAction = deleteQuery.delete
+            val deleteAction = deleteQuery.delete
 
-      // Print the SQL for the Coffees delete query
-      println("Generated SQL for Coffees delete:\n" + deleteAction.statements)
+            // Print the SQL for the Coffees delete query
+            println("Generated SQL for Coffees delete:\n" + deleteAction.statements)
 
-      // Perform the delete
-      db.run(deleteAction).map { numDeletedRows =>
-        println(s"Deleted $numDeletedRows rows")
-      }
+            // Perform the delete
+            db.run(deleteAction).map { numDeletedRows =>
+              println(s"Deleted $numDeletedRows rows")
+            }
 
-    }.flatMap { _ =>
+          }.flatMap { _ =>
 
-      /* Sorting / Order By */
+            /* Sorting / Order By */
 
-      val sortByPriceQuery: Query[Coffees, (String, Int, Double, Int, Int), Seq] =
-        coffees.sortBy(_.price)
+            val sortByPriceQuery: Query[Coffees, (String, Int, Double, Int, Int), Seq] =
+              coffees.sortBy(_.price)
 
-      println("Generated SQL for query sorted by price:\n" +
-        sortByPriceQuery.result.statements)
+            println("Generated SQL for query sorted by price:\n" +
+              sortByPriceQuery.result.statements)
 
-      // Execute the query
-      db.run(sortByPriceQuery.result).map(println)
+            // Execute the query
+            db.run(sortByPriceQuery.result).map(println)
 
-    }.flatMap { _ =>
+          }.flatMap { _ =>
 
-      /* Query Composition */
+            /* Query Composition */
 
-      val composedQuery: Query[Rep[String], String, Seq] =
-        coffees.sortBy(_.name).take(3).filter(_.price > 9.0).map(_.name)
+            val composedQuery: Query[Rep[String], String, Seq] =
+              coffees.sortBy(_.name).take(3).filter(_.price > 9.0).map(_.name)
 
-      println("Generated SQL for composed query:\n" +
-        composedQuery.result.statements)
+            println("Generated SQL for composed query:\n" +
+              composedQuery.result.statements)
 
-      // Execute the composed query
-      db.run(composedQuery.result).map(println)
+            // Execute the composed query
+            db.run(composedQuery.result).map(println)
 
-    }.flatMap { _ =>
+          }.flatMap { _ =>
 
-      /* Joins */
+            /* Joins */
 
-      // Join the tables using the relationship defined in the Coffees table
-      val joinQuery: Query[(Rep[String], Rep[String]), (String, String), Seq] = for {
-        c <- coffees if c.price > 9.0
-        s <- c.supplier
-      } yield (c.name, s.name)
+            // Join the tables using the relationship defined in the Coffees table
+            val joinQuery: Query[(Rep[String], Rep[String]), (String, String), Seq] = for {
+              c <- coffees if c.price > 9.0
+              s <- c.supplier
+            } yield (c.name, s.name)
 
-      println("Generated SQL for the join query:\n" + joinQuery.result.statements)
+            println("Generated SQL for the join query:\n" + joinQuery.result.statements)
 
-      // Print the rows which contain the coffee name and the supplier name
-      db.run(joinQuery.result).map(println)
+            // Print the rows which contain the coffee name and the supplier name
+            db.run(joinQuery.result).map(println)
 
-    }.flatMap { _ =>
+          }.flatMap { _ =>
 
-      /* Computed Values */
+            /* Computed Values */
 
-      // Create a new computed column that calculates the max price
-      val maxPriceColumn: Rep[Option[Double]] = coffees.map(_.price).max
+            // Create a new computed column that calculates the max price
+            val maxPriceColumn: Rep[Option[Double]] = coffees.map(_.price).max
 
-      println("Generated SQL for max price column:\n" + maxPriceColumn.result.statements)
+            println("Generated SQL for max price column:\n" + maxPriceColumn.result.statements)
 
-      // Execute the computed value query
-      db.run(maxPriceColumn.result).map(println)
+            // Execute the computed value query
+            db.run(maxPriceColumn.result).map(println)
 
-    }.flatMap { _ =>
+          }.flatMap { _ =>
 
-      /* Manual SQL / String Interpolation */
+            /* Manual SQL / String Interpolation */
 
-      // A value to insert into the statement
-      val state = "CA"
+            // A value to insert into the statement
+            val state = "CA"
 
-      // Construct a SQL statement manually with an interpolated value
-      val plainQuery = sql"select SUP_NAME from SUPPLIERS where STATE = $state".as[String]
+            // Construct a SQL statement manually with an interpolated value
+            val plainQuery = sql"select SUP_NAME from SUPPLIERS where STATE = $state".as[String]
 
-      println("Generated SQL for plain query:\n" + plainQuery.statements)
+            println("Generated SQL for plain query:\n" + plainQuery.statements)
 
-      // Execute the query
-      db.run(plainQuery).map(println)
+            // Execute the query
+            db.run(plainQuery).map(println)
 
-    }
-    Await.result(f, Duration.Inf)
-*/
+          }
+     */
+          Await.result(f, Duration.Inf)
 
   } finally dbConfig.db.close
 }
